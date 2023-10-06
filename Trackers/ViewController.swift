a5863dfaeb2efa1b77d6abae544a582c25e0a27d
zlavok322 <zarazazaza2010@gmail.com>
2023-10-06 19:26:15 +0300

    Initial Commit

---
+++ Trackers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trackers
+//
+//  Created by Слава Шестаков on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
